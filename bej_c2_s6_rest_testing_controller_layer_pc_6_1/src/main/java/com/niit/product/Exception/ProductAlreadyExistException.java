903fd636eb8afc171975657f9af2bd887b36717c
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-02-22 17:48:34 +0530

    Delete bej_c2_s6_rest_testing_controller_layer_pc_6_1 directory

--- bej_c2_s6_rest_testing_controller_layer_pc_6_1/src/main/java/com/niit/product/Exception/ProductAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.niit.product.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT,reason="Product Already Exists")
-public class ProductAlreadyExistException extends Exception{
-}
