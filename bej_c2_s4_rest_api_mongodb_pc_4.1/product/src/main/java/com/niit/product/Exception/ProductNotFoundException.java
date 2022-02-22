5be071afbb1cfb2b71ef3e4abd2ef6f3f1ce9a8f
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-02-22 17:48:22 +0530

    Delete bej_c2_s4_rest_api_mongodb_pc_4.1/product directory

--- bej_c2_s4_rest_api_mongodb_pc_4.1/product/src/main/java/com/niit/product/Exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.niit.product.Exception;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND,reason="Product doesn't exist")
-public class ProductNotFoundException extends Exception{
-}
